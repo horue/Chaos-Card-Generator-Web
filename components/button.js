b9ccb19bf1a73e28841b795ec301101047e606eb
Jorge <jorgejapao00@gmail.com>
2025-06-15 10:25:45 -0300

    custom button componente added

---
+++ components/button.js
@@ -0,0 +1,31 @@
+//Imports React
+import {Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+
+// Componente modelo
+export const CustomButton = ({ icon, buttonText, textAlign, onPress, buttonColor, textColor }) => (
+  <TouchableOpacity style={[styles.card, {backgroundColor: buttonColor}]} onPress={() => {try{onPress()}catch(error){console.log('No function defined')}}}>
+    <Text style={[styles.text, { textAlign, color: textColor, fontWeight: 'bold'  }]}>{buttonText.toUpperCase()}</Text>
+  </TouchableOpacity>
+);
+
+// Main Component
+export const styles = StyleSheet.create({
+    card: {
+        height: 40,
+        borderRadius: 18,
+        justifyContent: 'center',
+        alignItems: 'center', 
+        elevation: 2
+    },
+    center: {
+        fontSize: 14, 
+        fontWeight: 'bold', 
+        alignSelf: 'center', 
+    },
+    left: { 
+        fontSize: 14, 
+        fontWeight: 'bold', 
+        alignSelf: 'left', 
+    },
+});
