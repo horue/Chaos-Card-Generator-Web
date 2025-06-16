5a82dc592ecd656cc6dc341aedabba12ae4351d7
Jorge <jorgejapao00@gmail.com>
2025-06-16 18:27:34 -0300

    line break added to stroked text

--- components/strokedtext.js
+++ components/strokedtext.js
@@ -1,25 +1,32 @@
-import Svg, { Text as SvgText } from 'react-native-svg';
+import Svg, { Text as SvgText, TSpan } from 'react-native-svg';
 import { View } from 'react-native';
 
-
-export const StrokedText = ({ text, top, left, fontSize, strokeWidth, font }) => (
-  <View style={{ top, left, position: 'absolute' }}>
-    <Svg height={fontSize + 200} width={1500}>
-      <SvgText
-        stroke="black"
-        strokeWidth={strokeWidth}
-        fill="white"
-        fontSize={fontSize}
-        fontFamily={font}
-        x={4}
-        y={fontSize}
-        strokeLinejoin="round"
-        strokeLinecap="round"
-        paintOrder="stroke"
-        fillRule="nonzero"
-      >
-        {text}
-      </SvgText>
-    </Svg>
-  </View>
-);
+export const StrokedText = ({ text, top, left, fontSize, strokeWidth, font }) => {
+  const lines = text.split('\n');
+  
+  return (
+    <View style={{ top, left, position: 'absolute' }}>
+      <Svg height={fontSize + 600} width={1500}>
+        <SvgText
+          stroke="black"
+          strokeWidth={strokeWidth}
+          fill="white"
+          fontSize={fontSize}
+          fontFamily={font}
+          x={4}
+          y={fontSize}
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          paintOrder="stroke"
+          fillRule="nonzero"
+        >
+          {lines.map((line, i) => (
+            <TSpan key={i} x={4} dy={i === 0 ? 0 : fontSize * 1.2}>
+              {line}
+            </TSpan>
+          ))}
+        </SvgText>
+      </Svg>
+    </View>
+  );
+};
