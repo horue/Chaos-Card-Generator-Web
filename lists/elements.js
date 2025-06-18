ba1f861f9d18ad86f7cb974c870127fbaf3bcfe8
Jorge <jorgejapao00@gmail.com>
2025-06-18 16:07:50 -0300

    new elements added

--- lists/elements.js
+++ lists/elements.js
@@ -1,4 +1,6 @@
 export const images = {
     'nameless': require('../assets/ps_template.png'),
     'test': require('../assets/template.png'),
+    'dark': require('../assets/template_dark.png'),
+    'typeless': require('../assets/template_typeless.png'),
 };
