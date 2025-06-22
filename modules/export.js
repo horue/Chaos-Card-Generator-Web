fd4d0e37099478220f0f09284f530d6fd8f1a473
Jorge <jorgejapao00@gmail.com>
2025-06-22 09:37:54 -0300

    extension changed from txt to ctcg

--- modules/export.js
+++ modules/export.js
@@ -1,6 +1,6 @@
 export const exportHandler = (supertype, element, name, rank, info, effect, power, id, date) => {
   const content = `Superype: ${supertype}\nElement: ${element}\nName: ${name}\nRank: ${rank}\nCard Info: ${info}\nCard Effect: ${effect}\nPower: ${power}\nId: ${id}\nDate: ${date}`;
-  const filename = `${name}.txt`;
+  const filename = `${name}.ctcg`;
 
   const blob = new Blob([content], { type: 'text/plain' });
   const link = document.createElement('a');
