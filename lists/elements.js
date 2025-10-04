ad729b243fdd93a3e0e0004bcc7072c94b85d9c4
Jorge <jorgejapao00@gmail.com>
2025-10-04 14:15:23 -0300

    elements list updated

--- lists/elements.js
+++ lists/elements.js
@@ -3,7 +3,7 @@ export const images = {
     'test': require('../assets/template.png'),
     'dark': require('../assets/template_dark.png'),
     'typeless': require('../assets/template_typeless.png'),
-    'grass': require("../assets/template_grass"),
-    'fire': require("../assets/template_fire")
+    'grass': require("../assets/template_grass.png"),
+    'fire': require("../assets/template_fire.png"),
 
 };
