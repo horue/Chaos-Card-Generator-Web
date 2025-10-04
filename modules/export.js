8ee81550ad10e4d2869ea66b4df82529ba3ad7ff
Jorge <jorgejapao00@gmail.com>
2025-10-04 08:04:58 -0300

    export methods moved to a class

--- modules/export.js
+++ modules/export.js
@@ -1,39 +1,40 @@
-export const ctcgExportHandler = (supertype, element, name, rank, info, effect, power, id, date) => {
-  const content = `Superype: ${supertype}\nElement: ${element}\nName: ${name}\nRank: ${rank}\nCard Info: ${info}\nCard Effect: ${effect}\nPower: ${power}\nId: ${id}\nDate: ${date}`;
-  const filename = `${name}.ctcg`;
+export class Expoerter{
+  static ctcgExportHandler = (supertype, element, name, rank, info, effect, power, id, date) => {
+    const content = `Superype: ${supertype}\nElement: ${element}\nName: ${name}\nRank: ${rank}\nCard Info: ${info}\nCard Effect: ${effect}\nPower: ${power}\nId: ${id}\nDate: ${date}`;
+    const filename = `${name}.ctcg`;
 
-  const blob = new Blob([content], { type: 'text/plain' });
-  const link = document.createElement('a');
+    const blob = new Blob([content], { type: 'text/plain' });
+    const link = document.createElement('a');
 
-  link.href = URL.createObjectURL(blob);
-  link.download = filename;
-  link.click();
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
 
-  URL.revokeObjectURL(link.href);
-};
-
-export const JSONExportHandler = (supertype, element, name, rank, info, effect, power, id, date) =>{
-  const JSONFile = {
-    Supertype: supertype,
-    Element: element,
-    Name: name,
-    Rank: rank,
-    CardInfo: info,
-    CardEffect: effect,
-    Power: power,
-    Id: id,
-    Date: date
+    URL.revokeObjectURL(link.href);
   };
 
-  const filename = `${name}.json`;
+  static JSONExportHandler = (supertype, element, name, rank, info, effect, power, id, date) =>{
+    const JSONFile = {
+      Supertype: supertype,
+      Element: element,
+      Name: name,
+      Rank: rank,
+      CardInfo: info,
+      CardEffect: effect,
+      Power: power,
+      Id: id,
+      Date: date
+    };
 
-  const blob = new Blob([JSON.stringify(JSONFile, null, 2)], { type: 'application/json' });
-  const link = document.createElement('a');
+    const filename = `${name}.json`;
 
-  link.href = URL.createObjectURL(blob);
-  link.download = filename;
-  link.click();
+    const blob = new Blob([JSON.stringify(JSONFile, null, 2)], { type: 'application/json' });
+    const link = document.createElement('a');
 
-  URL.revokeObjectURL(link.href);
-};
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
 
+    URL.revokeObjectURL(link.href);
+  };
+}
