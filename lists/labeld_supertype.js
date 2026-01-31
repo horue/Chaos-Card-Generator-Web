a7b392087b1637410dff52d28a8599972f57d969
Jorge <jorgejapao00@gmail.com>
2026-01-31 20:59:47 -0300

    supertype list added to separeta file

---
+++ lists/labeld_supertype.js
@@ -0,0 +1,22 @@
+export class FullArt{
+
+    static creatures = [
+        { label: 'Typeless', value: 'typeless' },
+        { label: 'Fire', value: 'fire' },
+        { label: 'Water', value: 'water' },
+        { label: 'Grass', value: 'grass' },
+        { label: 'Dark', value: 'dark' },
+        { label: 'Light', value: 'light' },
+        { label: 'Rock', value: 'rock' },
+    ];
+
+    static spell = [
+        { label: 'Typeless', value: 'typeless_spell' },
+        { label: 'Fire', value: 'fire_spell' },
+        { label: 'Water', value: 'water_spell' },
+        { label: 'Grass', value: 'grass_spell' },
+        { label: 'Dark', value: 'dark_spell' },
+        { label: 'Light', value: 'light_spell' },
+        { label: 'Rock', value: 'rock_spell' },
+    ]
+}
