8c83c95bfde391705076e7718019ffe3b30c2cee
Jorge <jorgejapao00@gmail.com>
2026-01-31 20:46:42 -0300

    spell path added

--- lists/elements.js
+++ lists/elements.js
@@ -1,11 +1,20 @@
 export const images = {
-    'nameless': require('../assets/ps_template.png'),
+    'water': require('../assets/ps_template.png'),
     'test': require('../assets/template.png'),
     'dark': require('../assets/template_dark.png'),
     'typeless': require('../assets/template_typeless.png'),
     'grass': require("../assets/template_grass.png"),
     'fire': require("../assets/template_fire.png"),
     'light': require("../assets/template_light.png"),
-    'rock': require("../assets/template_rock.png")
+    'rock': require("../assets/template_rock.png"),
+
+    'water_spell': require('../assets/ps_template_spell.png'),
+    'dark_spell': require('../assets/template_dark_spell.png'),
+    'typeless_spell': require('../assets/template_typeless_spell.png'),
+    'grass_spell': require("../assets/template_grass_spell.png"),
+    'fire_spell': require("../assets/template_fire_spell.png"),
+    'light_spell': require("../assets/template_light_spell.png"),
+    'rock_spell': require("../assets/template_rock_spell.png")
+    
 
 };
